02c1ea1cb882af6c536331a3909c88239c255ab0
tinhvu11235 <tinhvu11235@gmail.com>
2025-11-02 15:31:39 +0700

    Update config.py
    Signed-off-by: tinhvu11235 <tinhvu11235@gmail.com>

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 Config = {
     "TRAIN_DATASET_PATH": "/kaggle/input/coco-stuff-image-only/train2017/train2017",
     "VAL_DATASET_PATH": "/kaggle/input/coco-stuff-image-only/val2017/val2017",
-    "BATCH_SIZE": 32,
+    "BATCH_SIZE": 16,
     "EPOCHS": 200,
     "LR_G": 4e-4,
     "LR_D": 2e-4,
